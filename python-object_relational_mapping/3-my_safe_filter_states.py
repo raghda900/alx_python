2429ddc1a0fc64f6a9d4ef949acb2707e4147829
raghda900 <raghda.soud90@gmail.com>
2024-01-04 14:16:28 +0200

    0

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+import MySQLdb
+from sys import argv
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+state_name     = argv[4]
+dbconnect = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, password=mysql_password, db=mysql_database)
+cursor = dbconnect.cursor()
+cursor.execute("SELECT * FROM states WHERE BINARY  name = %s ", [state_name])
+states = cursor.fetchall()
+for state in states:
+    print(state)
+# Close all cursors
+cursor.close()
+# Close all databases
+dbconnect.close()
