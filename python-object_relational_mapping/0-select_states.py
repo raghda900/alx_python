7fcd74bbe7797ef42bcd4f3aafaae7a281b93a77
raghda900 <raghda.soud90@gmail.com>
2024-01-04 13:56:06 +0200

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,8 @@ from sys import argv
 mysql_username=argv[1]
 mysql_password=argv[2]
 mysql_database=argv[3]
-dbconnect= mysqldb.connect(host='localhost',user=mysql_usename,password=mysql_password,db=mysql_database)
-#dbconnect= mysqldb.connect(host='localhost',port='3306',user=mysql_usename,password=mysql_password,db=mysql_database)
+dbconnect= mysqldb.connect(host='localhost',user=mysql_usename,passwd=mysql_password,db=mysql_database)
+#dbconnect= mysqldb.connect(host='localhost',port='3306',user=mysql_usename,passwd=mysql_password,db=mysql_database)
 cursor=dbconnect.cursor()
 cursor.execute('SELECT * FROM states')
 states= cursor.fetchall()
