68a5149098da3aed2b8137e97055b07390c64dca
raghda900 <raghda.soud90@gmail.com>
2024-01-04 14:02:17 +0200

    0

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+import MySQLdb
+from sys import argv
+
+mysql_username = argv[1]
+mysql_password = argv[2]
+mysql_database = argv[3]
+dbconnect = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, password=mysql_password, db=mysql_database)
+cursor = dbconnect.cursor()
+cursor.execute('SELECT * FROM states WHERE BINARY name LIKE "N%" ')
+states = cursor.fetchall()
+for state in states:
+    print(state)
